8ed6bc4bd0eabe593661a58ea36aa0c683d1b68a
Parvathavarthini <varthinibpv@gmail.com>
2026-03-26 12:44:00 -0400

    init commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("String Functions ()");
+        String literal = "Hello World";
+        String strObj = new String("Hello World");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("*** String Functions ***");
+
+        System.out.println("String literal : " + literal);
+        System.out.println("String Object : " + strObj);
+
+        System.out.println("String Object memory: " + strObj.hashCode());
+        System.out.println("String Object equals: " + literal.equals(strObj));
     }
 }
